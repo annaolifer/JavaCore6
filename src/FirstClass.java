385905b61687473fba2f88af7e855d14364d47e3
annaolifer <oliferanna@gmail.com>
2017-02-04 11:42:11 +0200

    First commit

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by home-pc on 2/2/2017.
+ */
+public class FirstClass {
+    public static void main(String[] args){
+        System.out.println("Мой первый класс на Java");
+    }
+
+  }
---
+++ src/ThirdClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by home-pc on 2/2/2017.
+ */
+public class ThirdClass {
+    public static void main(String[] args){
+        System.out.println("Мой первый класс на Java");
+    }
+}
